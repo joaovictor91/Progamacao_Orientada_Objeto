d036b53ff3cd4b69378e79de23391c767b73892d
joaovictor91 <j-victor-franco@live.com>
2022-04-23 16:16:47 -0300

    4º Modulo Continuação

---
+++ ConstrutorAluno/ConstrutorAluno/Alunos.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstrutorAluno
+{
+    class Alunos
+    {
+        private static int qdta = 0;
+        private static long ra = 1570482121001;
+        private string nome;
+        private int idade;
+        
+        public static long Ra
+        {
+            get
+            {
+                return ra;
+            }
+        }
+        public static int Qtda
+        {
+            get
+            {
+                return qdta;
+            }
+        }
+
+        static Alunos()
+        {
+             
+        }
+
+
+        //declaração dos metodos
+        public Alunos()
+        {
+
+        }
+
+        public Alunos(string nome)
+        {
+            qdta++;
+            ra++;
+            this.nome = nome;
+            
+        }
+
+        public Alunos( int idade)
+        {
+            ra++;
+            qdta++;
+            this.idade = idade;
+        }
+
+        public Alunos(string nome, int idade)
+        {
+            qdta++;
+            ra++;
+            this.nome = nome;
+            this.idade = idade;
+        }
+        
+
+
+
+        ////declaração das funções
+        public void MostrarAtributos()
+        {
+            Console.WriteLine("RA: " + ra + "\tNome: " + nome +
+                "\tIdade: " + idade);
+
+        }
+
+        public void MostrarQuantidadeAlunos()
+        {
+            Console.WriteLine("Quantidade de alunos: " + qdta);
+
+        }
+    }
+}
---
+++ ConstrutorAluno/ConstrutorAluno/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstrutorAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alunos q = new Alunos();
+
+            Alunos a1 = new Alunos("Maria de Fátima");
+            a1.MostrarAtributos();
+            Alunos a2 = new Alunos(30);
+            a2.MostrarAtributos();
+            Alunos a3 = new Alunos("João Victor Amaro Franco", 21);
+            a3.MostrarAtributos();
+            Alunos a4 = new Alunos("José",50);
+            a4.MostrarAtributos();
+            Alunos a5 = new Alunos("Carlos Eduardo");
+            a5.MostrarAtributos();
+            
+            q.MostrarQuantidadeAlunos();
+
+            Console.ReadKey();
+        }
+    }
+}
